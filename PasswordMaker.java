8a8a51a11fd70fc0935de6ce4eaf550ffae58d7b
ryoma okuda <catontheskate4423@gmail.com>
2021-06-11 20:57:23 +0900

    パスワード自動生成関数を作った

--- PasswordMaker.java
+++ PasswordMaker.java
@@ -1,5 +1,18 @@
+import java.util.Random;
+
 public class PasswordMaker{
     public static void main(String[]args){
         System.out.println("パスワードの長さを入力してください");
+    }
+
+    public String passmaker(int hoge) {
+        Random r = new Random();
+        String alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+        String _password = "";
+        for (int i = 0; i < hoge; i++) {
+            _password = _password + alphabet.charAt(r.nextInt(alphabet.length()));
+        }
+
+        return _password;
     }
 }
--- README.md
+++ README.md
@@ -3,3 +3,14 @@
 ## 開発者
 - kee111
 - jumang
+
+
+## kee111の実装
+
+- パスワードの長さを受け取る
+- その長さをstring passmaker(int hoge)を呼び出して、生成されたパスワードを表示する。
+
+## jumang
+
+- string passmaker(int hoge)を作る。
+
