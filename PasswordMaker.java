73173692580e640a778ee69267888b3d9ca25fe0
三浦桂 <kee@miurakatsuranoMacBook-Air.local>
2021-06-11 21:08:34 +0900

    あと数字だけ

--- PasswordMaker.java
+++ PasswordMaker.java
@@ -1,11 +1,18 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class PasswordMaker{
     public static void main(String[]args){
         System.out.println("パスワードの長さを入力してください");
+
+        Scanner sc = new Scanner(System.in);
+        int passwordLength = sc.nextInt();
+        System.out.println(passmaker(passwordLength));
+
+
     }
 
-    public String passmaker(int hoge) {
+    static String passmaker(int hoge) {
         Random r = new Random();
         String alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
         String _password = "";
